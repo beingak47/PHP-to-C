e4990ef19b9059c873ce9d53b973a3eb4f886e48
beingak47 <77290968+beingak47@users.noreply.github.com>
2021-01-11 22:26:16 +0530

    Update convert.php

--- convert.php
+++ convert.php
@@ -1,94 +1,87 @@
-#!/usr/bin/php
-<?php
+<?php 
 
-function showUsage()
-{
-    echo 'Usage: convert.php --if <inputfile.php> --of <output> [-v | --verbose] [-h | --help]' , "\n";
-}
-$debug = false;
-/* If there are arguments, cycle through em */
-if ($_SERVER['argc'] > 4) { 
-    for($i = 1; $i <= $_SERVER['argc']; ++$i) {
-        if(@$_SERVER['argv'][$i] == '-v' OR @$_SERVER['argv'][$i] == '--verbose') {
-            $debug = true;
-        }
-        if(@$_SERVER['argv'][$i] == '--if') {
-            $input = '';
-            $input = $_SERVER['argv'][$i + 1];
-        }
-        if(@$_SERVER['argv'][$i] == '--of') {
-            $output = '';
-            $output = $_SERVER['argv'][$i + 1];
-            $outputfile = $output . '.cpp';
-        }
-        if(@$_SERVER['argv'][$i] == '--help' OR @$_SERVER['argv'][$i] == '-h') {
-            showUsage();
+/**
+ * Logic assumption for employee of the week
+ * As there is 3 Parameter find highest score person for individual parameter
+ * Then Summarize score for all parameter that person gain as highest
+ * finally get highest score from all summarize parameter score
+ * person who get highest score considered as Employee of the week
+ */
+$emp_data_set = array(
+    0 => array(
+     'name' => 'Peter Parker',
+     'parameter' => array(
+         'attendance' => array(6,4,7,5,5),
+         'productivity' => array(3,4,3,5,5),
+         'client_feedback' => array(4,6,5,4,4)
+     )
+    ),
+    1 => array(
+        'name' => 'Steve Rogers',
+        'parameter' => array(
+            'attendance' => array(8,9,7,6,8),
+            'productivity' => array(3,4,8,6,5),
+            'client_feedback' => array(7,7,5,8,9)
+        )
+    ),
+    2 => array(
+            'name' => 'Peter Parker',
+            'parameter' => array(
+                'attendance' => array(3,4,6,4,5),
+                'productivity' => array(9,9,8,7,7),
+                'client_feedback' => array(6,7,6,5,6)
+            )
+    ),
+);
+
+$score_data = array();
+// iterate dataset to sum parameter wise score
+foreach ($emp_data_set as $key => $value) {
+    foreach ($value['parameter'] as $par_key => $par_val) {
+        foreach ($par_val as $day_key => $day_value) {
+            if(isset($score_data[$par_key][$key])){
+                $score_data[$par_key][$key] += $day_value;
+            }else {
+                $score_data[$par_key][$key] = $day_value;
+            }
         }
     }
-} else {
-    showUsage();
-    die ("Too Few Arguments\n");
 }
-
-if(!(isset($input) && strlen(trim($input)) && is_file($input))) {
-    showUsage();
-    die ($input . " isn't a file\n");
-}
-if(!(isset($output) && strlen(trim($output)))) { 
-    showUsage();
-    die ($output . "isn't a valid output file\n");
-}
-
-if($debug == true) {
-    define('DEBUG', true);
-} else {
-    define('DEBUG', false);
-}
-
-    if (substr(PHP_OS, 0, 3) == 'WIN') {
-		define('BINARYPHP_OS', 'Windows');
-	} else {
-		define('BINARYPHP_OS', 'Unix');
-	}
-	error_reporting(E_ALL);
-	if(!empty($_SERVER['DOCUMENT_ROOT']))
-		header('Content-Type: text/plain');
-	require_once 'tokenizer.php';
-	require_once 'tokenflow.php';
-	require_once 'functions.php';
-	$tokenizer = new Tokenizer();
-	$tokenizer->Parse($input);
-	$tokenizer->Strip();
-	$gen = new TokenGenerator($tokenizer);
-	list($code, $flags) = $gen->Convert();
-	if(DEBUG)
-		$flags .= ' -g';
-	$lines = explode("\n", $code);
-    if(DEBUG == true) {
-	foreach($lines as $line => $cod)
-		echo $line + 1, ': ', $cod, "\n";
+$person_param_score = array();
+//check higher score for each parameter (person wise and parameter wise)
+foreach ($score_data as $key => $value) {
+    $max_index = array_keys($value, max($value));
+    foreach ($max_index as $mkey => $mvalue) {
+        $person_param_score[$mvalue][] = $key;
     }
-	$fp = fopen($output . '.cpp', 'wt');
-	fwrite($fp, $code);
-	fclose($fp);
-	echo "\n\n", 'Compiling:', "\n";
-	touch('log');
-	if(BINARYPHP_OS == 'Windows') {
-		$comp = 'cl -c /nologo /MT /W3 /GX /FD -DWIN32 /Fotest.obj ' . $outputfile . ' >> log';
-		echo $comp, "\n\n";
-		shell_exec($comp);
-		if(DEBUG==true)
-			$opt='-debug';
-		else
-			$opt='-$comp';
-		$release = 'link ' .$output . '.obj -nologo ' .$opt .' -incremental:no -opt:ref '.$flags.' -out:' . $output . ' >> log';
-		echo $comp, "\n\n";
-		shell_exec($comp);
-	} else {
-		$comp = 'g++ -o ' . $output . ' ' .$outputfile. ' '. $flags . ' >> log 2>> log';
-		echo $comp, "\n\n";
-		shell_exec($comp);
-	}
-	echo implode(null, file('log'));
-	unlink('log');
+    
+}
+
+//calcualte total score based on higher parameter score
+$person_total_score = array();
+foreach ($person_param_score as $key => $value) {
+    foreach ($value as $pkey => $pvalue) {
+        //get parameter score
+        $param_score = $score_data[$pvalue][$key];
+        if(isset($person_total_score[$key])){
+            $person_total_score[$key] += $param_score;
+        }else {
+            $person_total_score[$key] = $param_score;
+        }
+    }
+}
+
+//find highest score of person
+$person_index = array_keys($person_total_score, max($person_total_score));
+
+if(count($person_index>0)){
+    foreach ($person_index as $key => $value) {
+        if(isset($emp_data_set[$value])){
+            echo "<br>Employee Of the Week is <b>".$emp_data_set[$value]['name']."</b>";
+        }
+    }
+}
+else {
+    echo "No one is applicable for Employee Of the Week";
+}
 ?>
